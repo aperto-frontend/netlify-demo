5d9a0065bce31d3b8cba21f6f7a92d91653484dd
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2019-03-26 08:26:18 +0100

    First init

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+
+/**
+ * Main entry file for styles.
+ */
+import './app.scss';
+
+/**
+ * Main entry file for application.
+ */
+import { Veams } from './app.veams';
+ 
+// Initialize modules with Veams
+// @INSERTPOINT :: @ref: js-init-modules-@1, @keep: true //
+// @INSERTPOINT :: @ref: js-init-once, @keep: true //
